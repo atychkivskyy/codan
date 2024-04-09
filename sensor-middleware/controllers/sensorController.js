8dc443202b020cdf8269b85af925ab88e4e6c399
alexdelacalle <116547430+alexdelacalle@users.noreply.github.com>
2024-04-09 20:01:11 +0200

    Creado la estructura de datos para el broker

--- sensor-middleware/controllers/sensorController.js
+++ sensor-middleware/controllers/sensorController.js
@@ -1,29 +1,51 @@
 const mqttClient = require('../services/mqttService');
+let timestamp = new Date().getTime();
+
 
 function test(req, res) {
     return res.status(200).send("Hello from Sensor Controller");
 }
 
 function retrieveTemperature(req, res) {
-    mqttClient.publish('sensor/temperature', JSON.stringify(req.body));
+    let temperature={
+        id: req.body.id_nodo,
+        temperature: req.body.temperatura,
+        time:timestamp
+    }
+    mqttClient.publish('sensor/temperature', JSON.stringify(temperature));
     return res.status(200).send("Temperature data received");
 }
 
 function retrieveHumidity(req, res) {
-    mqttClient.publish('sensor/humidity', JSON.stringify(req.body));
+    let humidity={
+        id: req.body.id_nodo,
+        humedad: req.body.humedad,
+        time:timestamp
+    }
+    mqttClient.publish('sensor/humidity', JSON.stringify(humidity));
     return res.status(200).send("Humidity data received");
 }
 
 function retrieveCO2(req, res) {
-    mqttClient.publish('sensor/co2', JSON.stringify(req.body));
+    let co2={
+        id: req.body.id_nodo,
+        temperature: req.body.co2,
+        time:timestamp
+    }
+    mqttClient.publish('sensor/co2', JSON.stringify(co2));
     return res.status(200).send("CO2 data received");
 }
 
-function retrieveOrganic(req, res) {
-    mqttClient.publish('sensor/organic', JSON.stringify(req.body));
+function retrieveVolatiles(req, res) {
+    let volatil={
+        id: req.body.id_nodo,
+        volatil: req.body.volatiles,
+        time:timestamp
+    }
+    mqttClient.publish('sensor/organic', JSON.stringify(volatil));
     return res.status(200).send("Organic data received");
 }
 
 module.exports = {
-    test, retrieveTemperature, retrieveHumidity, retrieveCO2, retrieveOrganic
+    test, retrieveTemperature, retrieveHumidity, retrieveCO2, retrieveVolatiles
 }
