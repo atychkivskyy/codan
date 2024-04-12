0a6087f88ce6b1fb32fa9ac117d4b38ef6ba3ccb
Andriy Tychkivskyy Yurchuk <a.tychkivskyy@alumnos.upm.es>
2024-04-12 14:17:34 +0200

    reformated ipfilter

--- sensor-middleware/app.js
+++ sensor-middleware/app.js
@@ -1,24 +1,13 @@
 const express = require("express");
 const sensorRoutes = require("./sensorRoutes");
 const morgan = require("morgan");
-const ipfilter = require('express-ipfilter').IpFilter;
+const ipfilter = require('express-ipfilter').IpFilter
 
+const blockedIPs = ['192.168.1.100', '10.0.2.2'];
 const PORT = process.env.SENSOR_MIDDLEWARE_PORT || 3000;
 const app = express();
 
-const blockedIPs = ['192.168.1.133'];
-const ipFilter = ipfilter(blockedIPs, {mode: 'deny'});
-
-app.use(ipFilter);
-
-app.use((err, req, res, next) => {
-    if (err instanceof ipfilter.IpDeniedError) {
-        res.status(403).send('Access denied');
-    } else {
-        next(err);
-    }
-});
-
+app.use(ipfilter(blockedIPs));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use("/", sensorRoutes);
