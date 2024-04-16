dee4b11ffdd901a8f24eea13beec9912a46af191
atychkivskyy <a.tychkivskyy@alumnos.upm.es>
2024-04-16 20:48:42 +0200

    adjusted ip

--- sensor-middleware/app.js
+++ sensor-middleware/app.js
@@ -3,7 +3,7 @@ const sensorRoutes = require("./sensorRoutes");
 const morgan = require("morgan");
 const ipfilter = require('express-ipfilter').IpFilter
 
-const blockedIPs = ['192.168.1.100', '10.0.2.2'];
+const blockedIPs = ['192.168.1.100', '10.0.3.21'];
 const PORT = process.env.SENSOR_MIDDLEWARE_PORT || 3000;
 const app = express();
 
