938ae3fcfb1107b55c735361cc7824a9f8af67e4
Andriy Tychkivskyy Yurchuk <a.tychkivskyy@alumnos.upm.es>
2024-04-17 13:49:25 +0200

    removed ipfilter from sensor-middleware

--- sensor-middleware/app.js
+++ sensor-middleware/app.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const sensorRoutes = require("./sensorRoutes");
 const morgan = require("morgan");
-const ipfilter = require('express-ipfilter').IpFilter
 
-const blockedIPs = ['192.168.1.100', '10.0.3.21'];
 const PORT = process.env.SENSOR_MIDDLEWARE_PORT || 3000;
 const app = express();
 
-app.use(ipfilter(blockedIPs));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use("/", sensorRoutes);
