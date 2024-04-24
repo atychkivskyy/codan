8d5774893a51dad7147b94794983760e063fb8ae
fegopri <159462660+fegopri@users.noreply.github.com>
2024-04-25 00:19:39 +0200

    Soap Version 3

--- soap/src/main/java/es/codan/springsoap/gen/package-info.java
+++ soap/src/main/java/es/codan/springsoap/gen/package-info.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por Eclipse Implementation of JAXB v3.0.0 
 // Visite https://eclipse-ee4j.github.io/jaxb-ri 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2024.04.24 a las 09:59:06 PM CEST 
+// Generado el: 2024.04.25 a las 12:12:31 AM CEST 
 //
 
 @jakarta.xml.bind.annotation.XmlSchema(namespace = "http://www.codan.es/springsoap/gen", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
