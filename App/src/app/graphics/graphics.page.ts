527eb6a6b2eca5e881c252e954286aecb994c37e
Borja Álvarez Bernardo <borja.alvarez@alumnos.upm.es>
2024-05-19 18:53:55 +0200

    ya se ven las graficas y boton

--- App/src/app/graphics/graphics.page.ts
+++ App/src/app/graphics/graphics.page.ts
@@ -24,7 +24,7 @@ export class GraphicsPage implements OnInit, AfterViewInit {
 
   temperature: number = 0;
   range_temp: string ="";
-  selectedSegment: string = "temperature";
+  selectedSegment: string = 'temperature';
 
   isAdmin: String = 'No'; //Para el menu
 
