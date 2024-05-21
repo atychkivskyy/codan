a794d6fc35478eefee82d9b7d8930c68f59ef9ee
atychkivskyy <a.tychkivskyy@alumnos.upm.es>
2024-05-21 18:40:33 +0200

    clean up

--- mysql-init/init-db.sql
+++
@@ -1,9 +0,0 @@
-USE app_users;
-
-CREATE TABLE users (
-    id          INT         AUTO_INCREMENT PRIMARY KEY,
-    username    VARCHAR(18) NOT NULL    UNIQUE,
-    password    VARCHAR(20) NOT NULL,
-    is_enabled  BOOLEAN     NOT NULL,
-    is_admin    BOOLEAN     NOT NULL
-)
